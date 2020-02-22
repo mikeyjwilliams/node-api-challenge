6e73e1f484ce6e1b91a8e998fee64e68c5a4cf7f
Michael Williams <manicdesigner@gmail.com>
2020-02-22 13:07:17 -0500

    add action router file chunk to projects-router

--- routes/projects-router.js
+++ routes/projects-router.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const projectModel = require('../data/helpers/projectModel');
+const actionRouter = require('./actions-router');
+
+router.use('/:id/actions', actionRouter);
 
 router.get('/', async (req, res, next) => {
   try {
