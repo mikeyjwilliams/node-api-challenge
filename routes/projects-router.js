767bbd3a76938bcaa87c498fdf26776a9c566ca2
Michael Williams <manicdesigner@gmail.com>
2020-02-22 12:38:51 -0500

    post

--- routes/projects-router.js
+++ routes/projects-router.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const router = express.Router();
-const actionModel = require('../data/helpers/actionModel');
+const projectModel = require('../data/helpers/projectModel');
 
 router.get('/', async (req, res, next) => {
   try {
-    const getActions = await actionModel.get();
-    res.status(200).json(getActions);
+    const getProjects = await projectModel.get();
+    res.status(200).json(getProjects);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  const { name, description } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: 'name required for project' });
+  }
+  if (!description) {
+    return res
+      .status(400)
+      .json({ message: 'description required for project' });
+  }
+  const projectPost = {
+    name: name,
+    description: description,
+    completed: req.body.completed || false,
+  };
+  try {
+    const post = await projectModel.insert(projectPost);
+    res.status(201).json(post);
   } catch (err) {
     console.log(err);
     next(err);
