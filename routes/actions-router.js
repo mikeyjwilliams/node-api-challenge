b9814891706fc22c103c3e6069d5e053f363697a
Michael Williams <manicdesigner@gmail.com>
2020-02-22 13:17:28 -0500

    post added

--- routes/actions-router.js
+++ routes/actions-router.js
@@ -11,4 +11,29 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+router.post('/', async (req, res, next) => {
+  const project_id = req.params.id;
+  const { description, notes } = req.body;
+
+  if (!description) {
+    res.status(400).json({ message: 'description required for action post' });
+  }
+  if (!notes) {
+    res.status(400).json({ message: 'notes required for action post' });
+  }
+  const actionPost = {
+    project_id: project_id,
+    description: description,
+    notes: notes,
+    completed: req.body.completed,
+  };
+  try {
+    const insertAction = await actionModel.insert(actionPost);
+    res.status(201).json(insertAction);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
 module.exports = router;
